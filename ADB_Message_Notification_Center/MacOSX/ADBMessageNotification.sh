7787e07e9b4708fae15c09c862653966ecb1a7e9
SimplyMinimal <SimplyMinimal@users.noreply.github.com>
2014-03-12 15:14:23 -0400

    Preparation for MacOSX cfg parser

---
+++ ADB_Message_Notification_Center/MacOSX/ADBMessageNotification.sh
@@ -0,0 +1,8 @@
+################## Functions ##################
+_Notify_Show () {
+growlnotify --image "Notify.ico" -t "ADB Message Notificiation Center" -m "New Message";
+}
+################# End Functions ##############
+
+
+_Notify_Show
