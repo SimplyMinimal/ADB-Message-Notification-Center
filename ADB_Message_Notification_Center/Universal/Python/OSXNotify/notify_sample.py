2d3c9a8519d02858e559e50d166a55c5c4a7e51b
SimplyMinimal <SimplyMinimal@users.noreply.github.com>
2014-03-19 16:06:21 -0400

    OSX SysTray Menu

---
+++ ADB_Message_Notification_Center/Universal/Python/OSXNotify/notify_sample.py
@@ -0,0 +1,34 @@
+import objc
+from Foundation import *
+from AppKit import *
+from PyObjCTools import AppHelper
+
+class MyApp(NSApplication):
+
+    def finishLaunching(self):
+        # Make statusbar item
+        statusbar = NSStatusBar.systemStatusBar()
+        self.statusitem = statusbar.statusItemWithLength_(NSVariableStatusItemLength)
+        self.icon = NSImage.alloc().initByReferencingFile_('icon.png')
+        self.icon.setScalesWhenResized_(True)
+        self.icon.setSize_((20, 20))
+        self.statusitem.setImage_(self.icon)
+
+        #make the menu
+        self.menubarMenu = NSMenu.alloc().init()
+
+        self.menuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('Click Me', 'clicked:', '')
+        self.menubarMenu.addItem_(self.menuItem)
+
+        self.quit = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('Quit', 'terminate:', '')
+        self.menubarMenu.addItem_(self.quit)
+
+        #add menu to statusitem
+        self.statusitem.setMenu_(self.menubarMenu)
+        self.statusitem.setToolTip_('My App')
+
+    def clicked_(self, notification):
+        NSLog('clicked!')
+
+if __name__ == "__main__":
+    app = MyApp.sharedApplication()
