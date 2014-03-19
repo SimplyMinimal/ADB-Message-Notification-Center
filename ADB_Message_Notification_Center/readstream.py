22b69bc86589258381e0984e3f5ddc1f715c7457
SimplyMinimal <SimplyMinimal@users.noreply.github.com>
2014-03-19 13:24:35 -0400

    Add PROGRAM_NAME and main

--- ADB_Message_Notification_Center/readstream.py
+++ ADB_Message_Notification_Center/readstream.py
@@ -1,8 +1,13 @@
 from subprocess import Popen, PIPE
+#Set Global Name
+PROGRAM_NAME = "ADB Message Notification Center"
 
+#Relaunches ADB in event of device disconnect
 def _ReLaunch():
     print "[+] Relaunching..."
     (stdout, stderr) = Popen(["ls","-la"], stdout=PIPE).communicate()
     print stdout
 
-_ReLaunch()
+if __name__ == "__main__":
+    print "-=="+PROGRAM_NAME+"==-"
+    _ReLaunch()
