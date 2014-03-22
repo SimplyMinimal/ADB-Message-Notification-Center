8a34e71f2041a196bcdbaec618b47ae24aaad50f
SimplyMinimal <SimplyMinimal@users.noreply.github.com>
2014-03-21 21:48:41 -0400

    check if file exists before creating cfg

--- ADB_Message_Notification_Center/Universal/Python/cfgParser.py
+++ ADB_Message_Notification_Center/Universal/Python/cfgParser.py
@@ -1,8 +1,13 @@
 import configparser
-config = configparser.ConfigParser()
-config.optionxform=str
-config['radio'] = {'New Message':'< SMS_ACKNOWLEDGE'}
-config['logcat'] = {'New TextPlus Message':'Displayed com.gogii.textplus'}
-with open('Notify.ini', 'w') as configfile:
-      config.write(configfile)
+import os.path
+#Global Variables
+INI_FILE = "Notify.ini"
+
+if not os.path.isfile(INI_FILE):
+    config = configparser.ConfigParser()
+    config.optionxform=str
+    config['radio'] = {'New Message':'< SMS_ACKNOWLEDGE'}
+    config['logcat'] = {'New TextPlus Message':'Displayed com.gogii.textplus'}
+    with open(INI_FILE, 'w') as configfile:
+        config.write(configfile)
 
